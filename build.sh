46df7681c641ad8bfc9fbb933e10bd969b8a1ec6
Will W <william86370@gmail.com>
2022-05-17 18:14:20 -0400

    chart Version 0.1.3

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.1.0
+VERSION=0.1.3
 
 # Create the helm package
 helm package ./vmware-cluster/
@@ -10,3 +10,12 @@ mv ./vmware-cluster-$VERSION.tgz ./helm-charts/
 
 # Make the index.yaml file
 helm repo index ./helm-charts/
+
+
+
+# Debug the chart 
+helm install  vmware-cluster ./vmware-cluster/  --values /./values-custom.yaml --dry-run --debug
+
+
+# Install the chart
+helm install  example-cluster ./vmware-cluster/  --values ./values-custom.yaml
